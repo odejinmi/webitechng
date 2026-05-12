9f4e05a8f893d7c688b188395e6db7ffd3f37b4d
videx <45696129+odejinmi@users.noreply.github.com>
2026-05-12 10:35:54 +0100

    sass

--- app/Notify/Email.php
+++ app/Notify/Email.php
@@ -14,157 +14,168 @@ use SendGrid\Mail\Mail;
 class Email extends NotifyProcess implements Notifiable
 {
 
-	/**
-	 * Email of receiver
-	 *
-	 * @var string
-	 */
-	public $email;
+    /**
+     * Email of receiver
+     *
+     * @var string
+     */
+    public $email;
 
-	/**
-	 * Assign value to properties
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->statusField = 'email_status';
-		$this->body = 'email_body';
-		$this->globalTemplate = 'email_template';
-		$this->notifyConfig = 'mail_config';
-	}
+    /**
+     * Assign value to properties
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->statusField = 'email_status';
+        $this->body = 'email_body';
+        $this->globalTemplate = 'email_template';
+        $this->notifyConfig = 'mail_config';
+    }
 
-	/**
-	 * Send notification
-	 *
-	 * @return void|bool
-	 */
-	public function send()
-	{
+    /**
+     * Send notification
+     *
+     * @return void|bool
+     */
+    public function send()
+    {
 
-		//get message from parent
-		$message = $this->getMessage();
-		if ($this->setting->en && $message) {
-			//Send mail
-			$methodName = $this->setting->mail_config->name;
-			$method = $this->mailMethods($methodName);
-			try {
-				$this->$method();
-				$this->createLog('email');
-			} catch (\Exception $e) {
-				$this->createErrorLog($e->getMessage());
-				session()->flash('mail_error', $e->getMessage());
-			}
-		}
-	}
+        //get message from parent
+        $message = $this->getMessage();
+        if ($this->setting->en && $message) {
+            //Send mail
+            $methodName = $this->setting->mail_config->name;
+            $method = $this->mailMethods($methodName);
 
-	/**
-	 * Get the method name
-	 *
-	 * @return string
-	 */
-	protected function mailMethods($name)
-	{
-		$methods = [
-			'php' => 'sendPhpMail',
-			'smtp' => 'sendSmtpMail',
-			'sendgrid' => 'sendSendGridMail',
-			'mailjet' => 'sendMailjetMail',
-		];
-		return $methods[$name];
-	}
+            \Log::info("Email Sending Attempt", [
+                'method' => $methodName,
+                'host' => $this->setting->mail_config->host ?? 'N/A',
+                'user' => $this->setting->mail_config->username ?? 'N/A',
+                'to' => $this->email
+            ]);
 
-	protected function sendPhpMail()
-	{
-		$general = $this->setting;
-		$headers = "From: $general->site_name <$general->email_from> \r\n";
-		$headers .= "Reply-To: $general->site_name <$general->email_from> \r\n";
-		$headers .= "MIME-Version: 1.0\r\n";
-		$headers .= "Content-Type: text/html; charset=utf-8\r\n";
-		@mail($this->email, $this->subject, $this->finalMessage, $headers);
-	}
+            try {
+                $this->$method();
+                $this->createLog('email');
+            } catch (\Exception $e) {
+                \Log::error("Email Sending Failed", [
+                    'error' => $e->getMessage()
+                ]);
+                $this->createErrorLog($e->getMessage());
+                session()->flash('mail_error', $e->getMessage());
+            }
+        }
+    }
 
-	protected function sendSmtpMail()
-	{
-		$mail = new PHPMailer(true);
-		$config = $this->setting->mail_config;
-		$general = $this->setting;
-		//Server settings
-		$mail->isSMTP();
-		$mail->Host       = $config->host;
-		$mail->SMTPAuth   = true;
-		$mail->Username   = $config->username;
-		$mail->Password   = $config->password;
-		if ($config->enc == 'ssl') {
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-		} else {
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-		}
-		$mail->Port       = $config->port;
-		$mail->CharSet = 'UTF-8';
-		//Recipients
-		$mail->setFrom($general->email_from, $general->site_name);
-		$mail->addAddress($this->email, $this->receiverName);
-		$mail->addReplyTo($general->email_from, $general->site_name);
-		// Content
-		$mail->isHTML(true);
-		$mail->Subject = $this->subject;
-		$mail->Body    = $this->finalMessage;
-		$mail->send();
-	}
+    /**
+     * Get the method name
+     *
+     * @return string
+     */
+    protected function mailMethods($name)
+    {
+        $methods = [
+            'php' => 'sendPhpMail',
+            'smtp' => 'sendSmtpMail',
+            'sendgrid' => 'sendSendGridMail',
+            'mailjet' => 'sendMailjetMail',
+        ];
+        return $methods[$name];
+    }
 
-	protected function sendSendGridMail()
-	{
-		$general = $this->setting;
-		$sendgridMail = new Mail();
-		$sendgridMail->setFrom($general->email_from, $general->site_name);
-		$sendgridMail->setSubject($this->subject);
-		$sendgridMail->addTo($this->email, $this->receiverName);
-		$sendgridMail->addContent("text/html", $this->finalMessage);
-		$sendgrid = new SendGrid($general->mail_config->appkey);
-		$response = $sendgrid->send($sendgridMail);
-		if ($response->statusCode() != 202) {
-			throw new Exception(json_decode($response->body())->errors[0]->message);
-		}
-	}
+    protected function sendPhpMail()
+    {
+        $general = $this->setting;
+        $headers = "From: $general->site_name <$general->email_from> \r\n";
+        $headers .= "Reply-To: $general->site_name <$general->email_from> \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+        @mail($this->email, $this->subject, $this->finalMessage, $headers);
+    }
 
-	protected function sendMailjetMail()
-	{
-		$general = $this->setting;
-		$mj = new Client($general->mail_config->public_key, $general->mail_config->secret_key, true, ['version' => 'v3.1']);
-		$body = [
-			'Messages' => [
-				[
-					'From' => [
-						'Email' => $general->email_from,
-						'Name' => $general->site_name,
-					],
-					'To' => [
-						[
-							'Email' => $this->email,
-							'Name' => $this->receiverName,
-						]
-					],
-					'Subject' => $this->subject,
-					'TextPart' => "",
-					'HTMLPart' => $this->finalMessage,
-				]
-			]
-		];
-		$response = $mj->post(Resources::$Email, ['body' => $body]);
-	}
+    protected function sendSmtpMail()
+    {
+        $mail = new PHPMailer(true);
+        $config = $this->setting->mail_config;
+        $general = $this->setting;
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = $config->host;
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $config->username;
+        $mail->Password   = $config->password;
+        if ($config->enc == 'ssl') {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        } else {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        }
+        $mail->Port       = $config->port;
+        $mail->CharSet = 'UTF-8';
+        //Recipients
+        $mail->setFrom($general->email_from, $general->site_name);
+        $mail->addAddress($this->email, $this->receiverName);
+        $mail->addReplyTo($general->email_from, $general->site_name);
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $this->subject;
+        $mail->Body    = $this->finalMessage;
+        $mail->send();
+    }
 
-	/**
-	 * Configure some properties
-	 *
-	 * @return void
-	 */
-	public function prevConfiguration()
-	{
-		if ($this->user) {
-			$this->email = $this->user->email;
-			$this->receiverName = $this->user->fullname;
-		}
-		$this->toAddress = $this->email;
-	}
+    protected function sendSendGridMail()
+    {
+        $general = $this->setting;
+        $sendgridMail = new Mail();
+        $sendgridMail->setFrom($general->email_from, $general->site_name);
+        $sendgridMail->setSubject($this->subject);
+        $sendgridMail->addTo($this->email, $this->receiverName);
+        $sendgridMail->addContent("text/html", $this->finalMessage);
+        $sendgrid = new SendGrid($general->mail_config->appkey);
+        $response = $sendgrid->send($sendgridMail);
+        if ($response->statusCode() != 202) {
+            throw new Exception(json_decode($response->body())->errors[0]->message);
+        }
+    }
+
+    protected function sendMailjetMail()
+    {
+        $general = $this->setting;
+        $mj = new Client($general->mail_config->public_key, $general->mail_config->secret_key, true, ['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => $general->email_from,
+                        'Name' => $general->site_name,
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $this->email,
+                            'Name' => $this->receiverName,
+                        ]
+                    ],
+                    'Subject' => $this->subject,
+                    'TextPart' => "",
+                    'HTMLPart' => $this->finalMessage,
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+    }
+
+    /**
+     * Configure some properties
+     *
+     * @return void
+     */
+    public function prevConfiguration()
+    {
+        if ($this->user) {
+            $this->email = $this->user->email;
+            $this->receiverName = $this->user->fullname;
+        }
+        $this->toAddress = $this->email;
+    }
 }
